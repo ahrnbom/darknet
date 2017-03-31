435042509f739485c04d7498f787f5c1d71598fa
Martin Ahrnbom <ahrnbom@maths.lth.se>
2017-03-31 16:11:11 +0200

    use new weights

--- scripts/valid.sh
+++ scripts/valid.sh
@@ -1,1 +1,1 @@
-./darknet detector valid cfg/miotcd.data cfg/yolo-miotcd.cfg miotcd.weights 2>&1 | tee output.log
+./darknet detector valid cfg/miotcd.data cfg/yolo-miotcd.cfg yolo-miotcd_170000.weights 2>&1 | tee output.log
