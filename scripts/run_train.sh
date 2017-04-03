1d5add9edba4b48074e5bb2c610d89204310efaa
Martin Ahrnbom <ahrnbom@maths.lth.se>
2017-04-03 14:22:53 +0200

    base it on yolo 2.0

--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -1,1 +1,1 @@
-stdbuf -o 0 nice -10 ./darknet detector train cfg/miotcd.data cfg/yolo-miotcd.cfg yolo.weights 2>&1 | tee output.log
+stdbuf -o 0 nice -10 ./darknet detector train cfg/miotcd.data cfg/yolo.2.0.miotcd.cfg yolo.weights 2>&1 | tee output.log
