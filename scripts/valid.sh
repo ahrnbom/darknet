65bcdbc2585f1d46ae2216cfd8778e6a3ec54beb
Martin Ahrnbom <ahrnbom@maths.lth.se>
2017-04-04 09:34:11 +0200

    Updated valid script for 2.0

--- scripts/valid.sh
+++ scripts/valid.sh
@@ -1,1 +1,1 @@
-./darknet detector valid cfg/miotcd.data cfg/yolo-miotcd.cfg backup/yolo-miotcd_final.weights 2>&1 | tee output_valid.log
+./darknet detector valid cfg/miotcd.data cfg/yolo.2.0.miotcd.cfg backup/yolo_final.weights 2>&1 | tee output_valid.log
