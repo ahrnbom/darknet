b106e3d93a260a8e0fa2c6cfd882090501399ca4
Martin Ahrnbom <ahrnbom@maths.lth.se>
2017-04-12 09:42:07 +0200

    store iou logs in separate folder

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,1 +1,1 @@
-./darknet detector iou cfg/$1.data cfg/yolo-miotcd.cfg backup/$1/$2.weights 2>&1 | tee output_test_$1_$2.log
+./darknet detector iou cfg/$1.data cfg/yolo-miotcd.cfg backup/$1/$2.weights 2>&1 | tee logs/output_test_$1_$2.log
