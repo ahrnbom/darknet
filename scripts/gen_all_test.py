64fb714453f3d4074cdc57b42f9a2cf7a7bc7c84
Martin Arhnbom <ma@maths.lth.se>
2017-04-12 09:39:48 +0200

    Fixed all_test

--- scripts/gen_all_test.py
+++ scripts/gen_all_test.py
@@ -4,4 +4,4 @@ a = np.arange(500400,503300,200).tolist()
 with open('scripts/all_test.sh', 'w+') as f:
     for i in range(0,5):
         for j in a:
-            f.write("./scripts/test.sh miotcd{} yolo_{}\n".format(i+1,j))
+            f.write("./scripts/test.sh miotcd{} yolo-miotcd_{}\n".format(i+1,j))
